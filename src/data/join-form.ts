070a9c8a417c1ad5f5ef4fa0e64562b2b589c460
marshot <124557537+marshot9420@users.noreply.github.com>
2023-12-03 19:24:55 +0900

    [Feat] 회원가입 더미 데이터 작성

---
+++ src/data/join-form.ts
@@ -0,0 +1,8 @@
+import { IJoinForm } from "../types";
+
+export const JoinForm1: IJoinForm = {
+  email: "user1@example.com",
+  name: "김회원",
+  password: "User1234!",
+  phone: "010-1234-5678",
+};
---
+++ src/types/api.request.types.ts
@@ -0,0 +1,6 @@
+export interface IJoinForm {
+  email: string;
+  name: string;
+  password: string;
+  phone: string;
+}
